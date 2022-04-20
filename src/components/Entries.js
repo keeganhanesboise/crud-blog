b9f1b7af93e538d12ffb52cd2de39ab6ecc52c28
keeganhanesboise <70166916+keeganhanesboise@users.noreply.github.com>
2022-04-20 16:19:34 -0600

    Added react to project

---
+++ src/components/Entries.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Entries extends Component {
+    render() {
+        return (
+            <div>
+                <p>ENTRIES DISPLAYED HERE</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/NewEntries.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class NewEntries extends Component {
+    render() {
+        return (
+            <div>
+                <p>CREATE ENTRIES HERE</p>
+            </div>
+        )
+    }
+}
